ad64816791cf7bf2505f55d6d81e14eea7781c8d
MichelRibeiro48 <michel-amoral@hotmail.com>
2022-04-28 21:23:12 -0300

    Changed names of variables for english

--- app.py
+++ app.py
@@ -9,76 +9,76 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:senhasecreta123@d
 
 db = SQLAlchemy(app)
 
-class Usuario(db.Model):
+class User(db.Model):
     rg = db.Column(db.Integer, primary_key= True, nullable=False)
-    nome = db.Column(db.String(50))
+    name = db.Column(db.String(50))
     email = db.Column(db.String(100))
-    senha = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     cpf = db.Column(db.String(14), nullable=False)
-    idade = db.Column(db.Integer, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
 
     def to_json(self):
-        return {"rg": self.rg, "nome": self.nome, "email": self.email,"senha": self.senha, "cpf": self.cpf, "idade": self.idade}
+        return {"rg": self.rg, "name": self.name, "email": self.email,"password": self.password, "cpf": self.cpf, "age": self.age}
 
-def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
+def func_response(status, content_name, content, message=False):
     body = {}
-    body[nome_do_conteudo] = conteudo
+    body[content_name] = content
 
-    if(mensagem):
-        body["mensagem"] = mensagem
+    if(message):
+        body["message"] = message
     
     return Response(json.dumps(body), status=status, mimetype="application/json")
-@app.route("/usuarios", methods=["GET"])
-def seleciona_usuarios(): # consulta ao banco de dados na tabela Usuario
-    usuarios_objetos = Usuario.query.all()
-    usuarios_json = [usuario.to_json() for usuario in usuarios_objetos]
+@app.route("/users", methods=["GET"])
+def select_all_users(): # consulta ao banco de dados na tabela Usuario
+    obj_users = User.query.all()
+    users_json = [user.to_json() for user in obj_users]
     
-    return gera_response(200, "usuarios", usuarios_json)
+    return func_response(200, "users", users_json)
 
-@app.route("/usuarios/<rg>", methods=["GET"])
-def seleciona_usuario(rg):
-    usuarios_objetos = Usuario.query.filter_by(rg=rg).first()
-    usuarios_json = usuarios_objetos.to_json()
+@app.route("/users/<rg>", methods=["GET"])
+def select_user(rg):
+    obj_user = User.query.filter_by(rg=rg).first()
+    users_json = obj_user.to_json()
 
-    return gera_response(200, "usuarios", usuarios_json)
+    return func_response(200, "user", users_json)
 
-@app.route("/usuarios", methods=["POST"])
+@app.route("/users", methods=["POST"])
 def cria_usuario():
     body = request.get_json()
 
     try:
-        usuario = Usuario(rg=body["rg"], nome=body["nome"], email= body["email"],senha= body["senha"],cpf= body["cpf"], idade= body["idade"])
-        if (usuario.idade >= 18):
-            db.session.add(usuario)
+        user = User(rg=body["rg"], name=body["name"], email= body["email"],password= body["password"],cpf= body["cpf"], age= body["age"])
+        if (user.age >= 18):
+            db.session.add(user)
             db.session.commit()      
         else:
-            return gera_response(400, "usuario", {}, "Erro ao cadastrar")
-        return gera_response(201, "usuario", usuario.to_json(), "Criado com sucesso")
+            return func_response(400, "usuario", {}, "Erro ao cadastrar")
+        return func_response(201, "usuario", user.to_json(), "Criado com sucesso")
     except Exception as e:
         print(e)
-        return gera_response(400, "usuario", {}, "Erro ao cadastrar")
+        return func_response(400, "usuario", {}, "Erro ao cadastrar")
 
-@app.route("/usuarios/<rg>", methods=["DELETE"])
+@app.route("/users/<rg>", methods=["DELETE"])
 def deleta_usuario(rg):
-    usuario_objeto = Usuario.query.filter_by(rg=rg).first()
+    obj_user = User.query.filter_by(rg=rg).first()
 
     try:
-        db.session.delete(usuario_objeto)
+        db.session.delete(obj_user)
         db.session.commit()
 
-        return gera_response(200, "usuario", usuario_objeto.to_json(), "deletado com sucesso")
+        return func_response(200, "usuario", obj_user.to_json(), "deletado com sucesso")
     except Exception as e:
         print("Erro", e)
-        return gera_response(400, "usuario", {}, "Erro ao deletar")
+        return func_response(400, "usuario", {}, "Erro ao deletar")
 
 @app.route("/login", methods=["POST"])
 def login():
     body = request.get_json()
-    usuario = Usuario.query.filter_by(cpf=body.cpf)
-    if (usuario.senha == body["password"]):
-        return usuario
+    user = User.query.filter_by(cpf=body.cpf)
+    if (user.password == body["password"]):
+        return user
     else:
-        return gera_response(400, "usuario", {}, "Senha invalida")
+        return func_response(400, "usuario", {}, "Senha invalida")
 app.run()
 
 
